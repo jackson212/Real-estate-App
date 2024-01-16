21c6f479cd0e73e23052d247990bf6304d6e9eef
jackson212 <jacksongeorge770@gmail.com>
2024-01-16 01:30:24 -0500

    created navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signout from './pages/Signout'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+import {
+
+  Routes,
+  Route,
+  BrowserRouter,
+ } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <h1 className='text-red-400'>App</h1>
-  )
+  return <BrowserRouter>
+       <Header/>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/signout' element={<Signout />} />
+      <Route path='/profile' element={<Profile />} />
+      
+       </Routes>
+  </BrowserRouter>;
+  
 }
 
 export default App
