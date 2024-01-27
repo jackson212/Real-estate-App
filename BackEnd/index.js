4244e5d6fdff5f89ad210c2be5cd8df3d2ad0fe8
jackson212 <jacksongeorge770@gmail.com>
2024-01-27 00:14:43 -0500

    added api router

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
+import UserRouter from './router/userRoute.js'
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL).then(()=>{
   console.log("database is connected")
@@ -14,7 +14,11 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 
 const app =express()
 
+app.use('/api/user',UserRouter)
+
 app.listen(3000,()=>{
    console.log(`app is running on port ${process.env.PORT}`)
-
+   
 })
+
+
